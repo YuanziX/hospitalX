30817f8aab40411a245e1ffc513d37026a2513a0
AtomicXZ <achubadyal4@gmail.com>
2023-10-28 12:14:56 +0530

    change font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hospitalx/app/pages/login_page/login_page.dart';
 
 void main() {
@@ -17,23 +18,25 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 240, 238, 105),
         ),
         useMaterial3: true,
-        textTheme: const TextTheme(
-          displaySmall: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
+        textTheme: GoogleFonts.notoSansTextTheme(
+          const TextTheme(
+            displaySmall: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            headlineMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
+            headlineSmall: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+            ),
+            titleLarge: TextStyle(
+              fontSize: 20,
+            ),
+            bodyMedium: TextStyle(),
           ),
-          headlineMedium: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
-          ),
-          headlineSmall: TextStyle(
-            fontWeight: FontWeight.normal,
-            fontSize: 20,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 20,
-          ),
-          bodyMedium: TextStyle(),
         ),
       ),
       home: const LoginPageProvider(),
